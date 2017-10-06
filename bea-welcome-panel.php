6da6048508d0ad7f47b0940d3d14b42b98590472
nkulka <nkulka@beapi.fr>
2017-10-06 11:06:16 +0200

    add filters

--- bea-welcome-panel.php
+++ bea-welcome-panel.php
@@ -41,37 +41,64 @@ class BEA_WELCOME_PANEL {
 	}
 
 	public static function beapi_welcome_panel() {
+		$title            = apply_filters( 'beapi_title_welcome_panel', __( 'Bienvenu sur votre tableau de bord", "bea-welcome-panel' ) );
+		$subtitle         = apply_filters( 'beapi_subtitle_welcome_panel', __( 'Conçu et réalisé par Be API, votre agence WordPress !', "bea-welcome-panel" ) );
+		$title_column_one = apply_filters( 'beapi_title_column_one_welcome_panel', __( 'Guides d\'utilisation', "bea-welcome-panel" ) );
+		$links_column_one = apply_filters( 'beapi_links_column_one_welcome_panel', array(
+			array(
+				'link'   => 'https://recette.beapi.fr/knowledge-base/utiliser-projecthuddle/',
+				'target' => '_blank',
+				'class'  => 'welcome-icon welcome-edit-page',
+				'title'  => __( 'Guide recette', 'bea-welcome-panel' )
+			)
+		) );
+		$html_column_one  = '';
+		if ( ! empty( $links_column_one ) ) {
+			$html_column_one .= '<ul>';
+			foreach ( $links_column_one as $link ) {
+				$html_column_one .= '<li><a href="' . $link['link'] . '" target="' . $link['target'] . '" class="' . $link['class'] . '">' . $link['title'] . '</a></li>';
+			}
+			$html_column_one .= '</ul>';
+		}
+		$title_column_two = apply_filters( 'beapi_title_column_two_welcome_panel', __( 'Support', 'bea-welcome-panel' ) );
+		$links_column_two = apply_filters( 'beapi_links_column_two_welcome_panel', array(
+			array(
+				'link'   => 'https://beapi.eu.teamwork.com',
+				'target' => '_blank',
+				'class'  => 'welcome-icon dashicons-admin-tools',
+				'title'  => __( 'Outil de gestion de Projet Be API', 'bea-welcome-panel' )
+			),
+			array(
+				'link'   => 'https://redmine.beapi.fr',
+				'target' => '_blank',
+				'class'  => 'welcome-icon dashicons-admin-tools',
+				'title'  => __( 'Outil de tickets TMA Be API', 'bea-welcome-panel' )
+			),
+			array(
+				'link'   => 'mailto:projets@beapi.fr',
+				'target' => '_blank',
+				'class'  => 'welcome-icon dashicons-admin-users',
+				'title'  => __( 'Support : projets@beapi.fr', 'bea-welcome-panel' )
+			)
+		) );
+		$html_column_two  = '';
+		if ( ! empty( $links_column_two ) ) {
+			$html_column_two .= '<ul>';
+			foreach ( $links_column_two as $link ) {
+				$html_column_two .= '<li><a href="' . $link['link'] . '" target="' . $link['target'] . '" class="' . $link['class'] . '">' . $link['title'] . '</a></li>';
+			}
+			$html_column_two .= '</ul>';
+		}
+		$img_src = apply_filters( 'beapi_logo_src_welcome_panel', plugin_dir_url( __FILE__ ) . 'assets/img/beapi.png' );
+		$logo    = apply_filters( 'beapi_logo_welcome_panel', '<a href="https://beapi.fr/" target="_blank"><img src="' . $img_src . '" alt="Be API" /></a>' );
 
-		$src     = plugin_dir_url( __FILE__ ) . 'assets/img/beapi.png';
-		$img_src = apply_filters( 'beapi_logo_welcome_panel', $src );
-
-		echo '
-		<div class="welcome-panel-content">
-			<h2>' . __( "Bienvenu sur votre tableau de bord", "bea-welcome-panel" ) . '</h2>
-			<p class="about-description">' . __( "Conçu et réalisé par Be API, votre agence WordPress !", "bea-welcome-panel" ) . '</p>
+		echo '<div class="welcome-panel-content">
+			<h2>' . $title . '</h2>
+			<p class="about-description">' . $subtitle . '</p>
 			<div class="welcome-panel-column-container">
-				<div class="welcome-panel-column">
-					<h3>' . __( "Guides d'utilisation", "bea-welcome-panel" ) . '</h3>
-					<ul>
-						<li><a href="https://recette.beapi.fr/knowledge-base/utiliser-projecthuddle/" target="_blank" class="welcome-icon welcome-edit-page">Guide recette</a></li>
-						<li><a href="" class="welcome-icon welcome-add-page"></a></li>
-						<li><a href="" class="welcome-icon welcome-write-blog"></a></li>
-						<li><a href="" class="welcome-icon welcome-view-site"></a></li>
-					</ul>
-				</div>
-				<div class="welcome-panel-column">
-					<h3>Support</h3>
-					<ul>
-						<li><a href="https://beapi.eu.teamwork.com" target="_blank" class="welcome-icon dashicons-admin-tools">' . __( "Outil de tickets Projet Be API", "bea-welcome-panel" ) . '</a></li>
-						<li><a href="https://redmine.beapi.fr" target="_blank" class="welcome-icon dashicons-admin-tools">' . __( "Outil de tickets TMA Be API", "bea-welcome-panel" ) . '</a></li>
-						<li><a href="mailto:projets@beapi.fr" target="_blank" class="welcome-icon dashicons-admin-users">' . __( "Support : projets@beapi.fr", "bea-welcome-panel" ) . '</a></li>
-					</ul>
-				</div>
-				<div class="welcome-panel-column welcome-panel-last">
-					<a href="https://beapi.fr/" target="_blank">
-							<img src="' . $img_src . '" alt="Be API" />
-						</a>
-				</div>
+				<div class="welcome-panel-column"><h3>' . $title_column_one . '</h3>' . $html_column_one . '</div>
+				<div class="welcome-panel-column"><h3>' . $title_column_two . '</h3>' . $html_column_two . '</div>
+				<div class="welcome-panel-column welcome-panel-last">' . $logo . '</div>
 			</div>
 		</div>';
 	}
